adc090028e1fbd7048c573212d793eceaec66678
Aly <alytaha46@gmail.com>
2023-01-04 21:24:52 +0200

    finish Select Location Fragment

--- app/src/main/java/com/udacity/project4/base/BaseFragment.kt
+++ app/src/main/java/com/udacity/project4/base/BaseFragment.kt
@@ -1,9 +1,16 @@
 package com.udacity.project4.base
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.util.Log
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.material.snackbar.Snackbar
 
 /**
@@ -14,6 +21,181 @@ abstract class BaseFragment : Fragment() {
      * Every fragment has to have an instance of a view model that extends from the BaseViewModel
      */
     abstract val _viewModel: BaseViewModel
+    var dialog: AlertDialog? = null
+    val fineLocationRequestCode = 1000
+    val fineAndBackgroundLocationRequestCode = 1001
+    val osVersionAboveQ = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q
+    val fineAndBackgroundLocationPermissions = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_BACKGROUND_LOCATION
+    )
+    val fineAndBackgroundLocationPermissionsMessages = arrayOf(
+        "current location and Background Location",
+        "Background location"
+    )
+    val fineLocationPermission = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+    val fineLocationPermissionMessages = arrayOf(
+        "current location and Background Location"
+    )
+
+
+    fun showDialog(
+        title: String? = null,
+        message: String? = null,
+        posActionName: String? = null,
+        posAction: DialogInterface.OnClickListener? = null,
+        negActionName: String? = null,
+        negAction: DialogInterface.OnClickListener? = null,
+        cancelable: Boolean = true
+    ) {
+        dialog = AlertDialog.Builder(
+            this.context
+        )
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton(posActionName, posAction)
+            .setNegativeButton(negActionName, negAction)
+            .setCancelable(cancelable)
+            .setIcon(android.R.drawable.ic_dialog_alert)
+            .show()
+    }
+
+    fun hideDialog() {
+        dialog?.dismiss()
+    }
+
+    fun isPermissionGranted(permissions: Array<String>): Boolean {
+        permissions.forEach {
+            if (ContextCompat.checkSelfPermission(
+                    this.requireContext(),
+                    it
+                ) == PackageManager.PERMISSION_DENIED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
+    /*
+        fun requestLocationPermission(doAction:()->Unit) {
+            if (osVersionAboveQ) {
+                if (!isLocationGranted(fineAndBackgroundLocationPermissions)) {
+                    if (shouldShowRequestPermissionRationale(
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        )
+                    ) {
+                        showDialog(
+                            "Important Permission",
+                            "Please Accept this Permission to access current location and Background Location",
+                            "OK",
+                            DialogInterface.OnClickListener { dialog, which ->
+                                hideDialog()
+                                requestPermissions(fineAndBackgroundLocationPermissions, fineAndBackgroundLocationRequestCode)
+                            },
+                            "Cancel",
+                            DialogInterface.OnClickListener { dialog, which ->
+                                hideDialog()
+                            }
+                        )
+                    } else if (shouldShowRequestPermissionRationale(
+                            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                        )
+                    ) {
+                        showDialog(
+                            "Important Permission",
+                            "Please Accept this Permission to access Background location",
+                            "OK",
+                            DialogInterface.OnClickListener { dialog, which ->
+                                hideDialog()
+                                requestPermissions(fineAndBackgroundLocationPermissions, fineAndBackgroundLocationRequestCode)
+                            },
+                            "Cancel",
+                            DialogInterface.OnClickListener { dialog, which ->
+                                hideDialog()
+                            }
+                        )
+                    } else {
+                        requestPermissions(fineAndBackgroundLocationPermissions, fineAndBackgroundLocationRequestCode)
+                    }
+                } else {
+                    doAction()
+                }
+
+            } else {
+                if (!isLocationGranted(fineLocationPermission)) {
+                    if (shouldShowRequestPermissionRationale(
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        )
+                    ) {
+                        showDialog(
+                            "Important Permission",
+                            "Please Accept this Permission to your location",
+                            "OK",
+                            DialogInterface.OnClickListener { dialog, which ->
+                                hideDialog()
+                                requestPermissions(fineLocationPermission, fineLocationRequestCode)
+                            },
+                            "Cancel",
+                            DialogInterface.OnClickListener { dialog, which ->
+                                hideDialog()
+                            }
+                        )
+                    } else {
+                        requestPermissions(fineLocationPermission, fineLocationRequestCode)
+                    }
+                } else {
+                    doAction()
+                }
+            }
+
+        }*/
+    fun requestPermission(
+        permissions: Array<String>,
+        requestCode: Int,
+        messages: Array<String>,
+        doAction: () -> Unit,
+        showSnackBarWhenDenied:() -> Unit
+    ) {
+        if (!isPermissionGranted(permissions)) {
+            var shouldShowPermissionRequest = true
+            permissions.forEachIndexed { index, it ->
+                if (shouldShowRequestPermissionRationale(it)
+                ) {
+                    showDialog(
+                        "Important Permission",
+                        "Please Accept this Permission to access ${messages[index]}",
+                        "OK",
+                        DialogInterface.OnClickListener { dialog, which ->
+                            hideDialog()
+                            requestPermissions(
+                                permissions,
+                                requestCode
+                            )
+                        },
+                        "Cancel",
+                        DialogInterface.OnClickListener { dialog, which ->
+                            hideDialog()
+                            showSnackBarWhenDenied()
+                        }
+                    )
+                    shouldShowPermissionRequest = false
+                    return
+                }
+            }
+            if (shouldShowPermissionRequest) {
+                requestPermissions(
+                    permissions,
+                    requestCode
+                )
+            }
+        } else {
+            doAction()
+        }
+
+    }
 
     override fun onStart() {
         super.onStart()
--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -2,16 +2,25 @@ package com.udacity.project4.locationreminders.savereminder.selectreminderlocati
 
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.content.res.Resources
+import android.location.Geocoder
+import android.location.Location
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.view.*
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
@@ -21,6 +30,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 import com.google.android.material.snackbar.Snackbar
+import com.udacity.project4.BuildConfig
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.base.NavigationCommand
@@ -28,12 +38,19 @@ import com.udacity.project4.databinding.FragmentSelectLocationBinding
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
+import java.util.*
 
-class SelectLocationFragment : BaseFragment() {
+class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
 
     //Use Koin to get the view model of the SaveReminder
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSelectLocationBinding
+    private lateinit var map: GoogleMap
+    private lateinit var marker: Marker
+    private lateinit var pointOfInterest: PointOfInterest
+    lateinit var fusedLocationClient: FusedLocationProviderClient
+    val REQUEST_TURN_DEVICE_LOCATION_ON = 101
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -46,23 +63,29 @@ class SelectLocationFragment : BaseFragment() {
 
         setHasOptionsMenu(true)
         setDisplayHomeAsUpEnabled(true)
-
-//        TODO: add the map setup implementation
-//        TODO: zoom to the user location after taking his permission
-//        TODO: add style to the map
-//        TODO: put a marker to location that the user selected
-
-
-//        TODO: call this function after the user confirms on the selected location
-        onLocationSelected()
-
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map_view) as SupportMapFragment
+        mapFragment.let {
+            mapFragment.getMapAsync(this)
+        }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        binding.saveButton.setOnClickListener {
+            onLocationSelected()
+        }
         return binding.root
     }
 
+
     private fun onLocationSelected() {
-        //        TODO: When the user confirms on the selected location,
-        //         send back the selected location details to the view model
-        //         and navigate back to the previous fragment to save the reminder and add the geofence
+        if (::pointOfInterest.isInitialized) {
+            _viewModel.setPOILocation(pointOfInterest)
+            findNavController().navigate(SelectLocationFragmentDirections.actionSelectLocationFragmentToSaveReminderFragment())
+        } else {
+            Snackbar.make(
+                this.view!!,
+                "No Location Selected",
+                Snackbar.LENGTH_LONG
+            ).show()
+        }
     }
 
 
@@ -71,21 +94,209 @@ class SelectLocationFragment : BaseFragment() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        // TODO: Change the map type based on the user's selection.
         R.id.normal_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
             true
         }
         R.id.hybrid_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_HYBRID
             true
         }
         R.id.satellite_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
             true
         }
         R.id.terrain_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_TERRAIN
             true
         }
         else -> super.onOptionsItemSelected(item)
     }
 
+    override fun onMapReady(gMap: GoogleMap) {
+        map = gMap
+        setPointClick(map)
+        setPoiClick(map)
+        setMapStyle(map)
+        enableMyLocation()
+    }
 
+    private fun setPointClick(map: GoogleMap) {
+        map.setOnMapClickListener {
+            if (::marker.isInitialized)
+                marker.remove()
+            val geocoder = Geocoder(this.context, Locale.getDefault())
+            val addresses = geocoder.getFromLocation(it.latitude, it.longitude, 1)
+            val address = addresses[0].getAddressLine(0)
+            marker = map.addMarker(
+                MarkerOptions()
+                    .position(it)
+                    .title(address)
+            )
+            pointOfInterest = PointOfInterest(it, null, address)
+            marker.showInfoWindow()
+        }
+    }
+
+    private fun setMapStyle(map: GoogleMap) {
+        try {
+            // Customize the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            val success = map.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    this.context,
+                    R.raw.map_style
+                )
+            )
+
+            if (!success) {
+                Log.e("setMapStyle", "Style parsing failed.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Log.e("setMapStyle", "Can't find style. Error: ", e)
+        }
+    }
+
+    fun enableMyLocation() {
+        if (osVersionAboveQ) {
+            requestPermission(
+                fineAndBackgroundLocationPermissions,
+                fineAndBackgroundLocationRequestCode,
+                fineAndBackgroundLocationPermissionsMessages,
+                ::getUserLocation,
+                ::showSnackBarWhenDenied
+            )
+        } else {
+            requestPermission(
+                fineLocationPermission,
+                fineLocationRequestCode,
+                fineLocationPermissionMessages,
+                ::getUserLocation,
+                ::showSnackBarWhenDenied
+            )
+        }
+    }
+
+
+    private fun setPoiClick(map: GoogleMap) {
+        map.setOnPoiClickListener { poi ->
+            if (::marker.isInitialized)
+                marker.remove()
+            marker = map.addMarker(
+                MarkerOptions()
+                    .position(poi.latLng)
+                    .title(poi.name)
+            )
+            marker.showInfoWindow()
+            pointOfInterest = poi
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQUEST_TURN_DEVICE_LOCATION_ON -> {
+                checkGPSIsOn(false)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+
+        if (requestCode == fineAndBackgroundLocationRequestCode) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                getUserLocation()
+            } else showSnackBarWhenDenied()
+        } else if (requestCode == fineLocationRequestCode) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getUserLocation()
+            } else showSnackBarWhenDenied()
+        }
+    }
+
+    fun showSnackBarWhenDenied() {
+        Snackbar.make(
+            requireActivity().findViewById(android.R.id.content),
+            R.string.location_required_error,
+            Snackbar.LENGTH_INDEFINITE
+        )
+            .setAction(R.string.settings) {
+                startActivity(Intent().apply {
+                    action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                    data = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null)
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                })
+            }.show()
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getUserLocation() {
+        map.isMyLocationEnabled = true
+        checkGPSIsOn(false)
+    }
+
+    @SuppressLint("MissingPermission")
+    fun checkGPSIsOn(enableBox: Boolean) {
+        val locationRequest = LocationRequest.create().apply {
+            priority = LocationRequest.PRIORITY_LOW_POWER
+        }
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+
+        val settingsClient = LocationServices.getSettingsClient(requireActivity())
+        val locationSettingsResponseTask =
+            settingsClient.checkLocationSettings(builder.build())
+        locationSettingsResponseTask.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException && enableBox) {
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    startIntentSenderForResult(
+                        exception.resolution.intentSender,
+                        REQUEST_TURN_DEVICE_LOCATION_ON,
+                        null,
+                        0,
+                        0,
+                        0,
+                        null
+                    )
+                    /*exception.startResolutionForResult(
+                        this.activity,
+                        REQUEST_TURN_DEVICE_LOCATION_ON
+                    )*/
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    Log.d("TAG", "Error geting location settings resolution: " + sendEx.message)
+                }
+            } else {
+                Snackbar.make(
+                    this.view!!,
+                    R.string.location_required_error,
+                    Snackbar.LENGTH_LONG
+                ).setAction(android.R.string.ok) {
+                    checkGPSIsOn(true)
+                }.show()
+            }
+
+        }
+        locationSettingsResponseTask.addOnCompleteListener {
+            if (it.isSuccessful) {
+                fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                    if (location != null) {
+                        val latLng = LatLng(location.latitude, location.longitude)
+                        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16f))
+                    }
+                }
+            }
+        }
+    }
 }
+
+
+
